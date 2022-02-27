4bdc0bab267b0df0ae8f0c6b7b255eea09155158
DanVlf <daniel.volf01@gmail.com>
2022-02-27 21:02:10 +0100

    first commit

---
+++ my-app/pages/index.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/navbar";
+import Hero from "../components/hero";
+import Pricing from "../components/pricing";
+import Footer from "../components/footer";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Pricing />
+      <Footer />
+    </div>
+    
+  );
+}
